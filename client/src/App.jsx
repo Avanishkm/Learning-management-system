12bc677e3a1797ec8e618bc937dda1184780586b
Avanish kumar <avanish.kumar@attplgroup.com>
2024-11-28 20:44:02 +0530

    added profile section and some files more

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
 import Login from "./pages/Login";
 import HeroSection from "./pages/student/HeroSection";
+import Courses from "./pages/student/Courses";
+import MyLearning from "./pages/student/MyLearning";
+import Profile from "./pages/student/Profile"
 
 function App() {
   const appRouter = createBrowserRouter([
@@ -15,16 +18,25 @@ function App() {
           element: (
             <>
               <HeroSection />
-              {/* Courses */}
+              <Courses/>
             </>
           ),
         },
+        {
+          path: "login",
+          element: <Login />,
+        },
+        {
+          path: "my-learning",
+          element: <MyLearning />,
+        },
+        {
+          path: "profile",
+          element: <Profile />,
+        },
       ],
     },
-    {
-      path: "login",
-      element: <Login />,
-    },
+    
   ]);
   return (
     <main>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -22,13 +22,14 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from "./sheet";
+} from "./ui/sheet";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const user = true;
   
   return (
-    <div className="h-16 dark:bg-[#0A0A0A] bg-white border-b dark:border-b-gray-800 border-b-gray-200 fixed top-0 left-0 right-0 duration-300 z-10">
+    <div className="h-16 dark:bg-[#020817] bg-white border-b dark:border-b-gray-800 border-b-gray-200 fixed top-0 left-0 right-0 duration-300 z-10">
       {/* Desktop */}
       <div className="max-w-7xl mx-auto hidden md:flex justify-between items-center gap-10 h-full">
         <div className="flex items-center gap-2">
@@ -55,19 +56,21 @@ const Navbar = () => {
                 <DropdownMenuSeparator />
                 <DropdownMenuGroup>
                   <DropdownMenuItem>
-                    <span>My Learning</span>
+                    <Link to="my-learning">My Learning</Link>
+                      
                   </DropdownMenuItem>
                   <DropdownMenuItem>
-                    <span>Edit Profile</span>
+                  <Link to="profile">Edit Profile</Link>
+                  
                   </DropdownMenuItem>
                   <DropdownMenuItem>
-                    <span>Log out</span>
+                  Log out
                   </DropdownMenuItem>
                 </DropdownMenuGroup>
                 <DropdownMenuSeparator />
                 <DropdownMenuSeparator />
                 <DropdownMenuItem>
-                  <span>Dashboard</span>
+                Dashboard
                 </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
